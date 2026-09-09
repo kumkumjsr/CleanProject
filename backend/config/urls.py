9817e58e0381f6db9e8008d6e9d73b3a7abf985a
Kumkum <kumkum123choti@gmail.com>
2026-09-09 21:39:02 +0530

    Fix production QR media serving

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf import settings
-from django.conf.urls.static import static
+from django.views.static import serve
 
 from accounts.views import LoginView
 from rest_framework_simplejwt.views import TokenRefreshView
@@ -64,8 +64,13 @@ urlpatterns = [
 ]
 
 
-# Serve media files
-urlpatterns += static(
-    settings.MEDIA_URL,
-    document_root=settings.MEDIA_ROOT
-)
+# Serve uploaded media files
+urlpatterns += [
+    re_path(
+        r"^media/(?P<path>.*)$",
+        serve,
+        {
+            "document_root": settings.MEDIA_ROOT,
+        },
+    ),
+]
